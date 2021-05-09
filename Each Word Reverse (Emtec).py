7a4b00b49d341eebab37eb68fd2108296b3538a0
akhileshmagdum <akhileshmagdum@gmail.com>
2021-05-09 09:08:07 +0530

    Added helpful comments

--- Each Word Reverse (Emtec).py
+++ Each Word Reverse (Emtec).py
@@ -1,6 +1,8 @@
 string = list(input().split())
 answer = ""
+
 for i in string:
     answer += i[::-1]
     answer +=" "
+
 print(answer)
--- largest number divisble by 3 (Emtec).py
+++ largest number divisble by 3 (Emtec).py
@@ -1,8 +1,10 @@
 numbers = list(map(int,input().split()))
 divisble3 = []
+
 for i in numbers:
     if i%3==0:
         divisble3.append(i)
     else:
         pass
+
 print(max(divisble3))
